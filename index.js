af910c18ddda3a7f273cc2f6db6937a5d4e2b790
Chris <orthagonal1@gmail.com>
2016-12-12 16:07:01 -0600

    pass method name, add str2fn dependency, lint fixes

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ const str2fn = require('str2fn');
 // for acceptable formats. this plugin always assumes a seconds field
 // is present for cron.
 exports.register = function(server, options, next) {
-  const onStart = () => {
-    str2fn(server.methods, options.onStart, false)();
+  const onStart = (methodName) => {
+    if (options.onStart) {
+      str2fn(server.methods, options.onStart, false)(methodName);
+    }
   };
-  const onEnd = (err, params) => {
-    str2fn(server.methods, options.onEnd, false)(err, params);
+  const onEnd = (err, methodName, params) => {
+    if (options.onEnd) {
+      str2fn(server.methods, options.onEnd, false)(err, methodName, params);
+    }
   };
 
   // this will hold the method, params and interval for each method we want to run:
@@ -24,67 +28,66 @@ exports.register = function(server, options, next) {
     const params = scheduleRequest.params ? scheduleRequest.params : [];
     const scheduleText = scheduleRequest.time ? scheduleRequest.time : scheduleRequest.cron;
 
-    if (!method){
-      return next(new Error('Method ' + scheduleRequest.method + ' not defined'));
+    if (!method) {
+      return next(new Error(`Method ${scheduleRequest.method} not defined`));
     }
 
-    if (!scheduleText){
-      return next(new Error('Method ' + scheduleRequest.method + ' requires either a valid "time" or "cron" schedule'));
+    if (!scheduleText) {
+      return next(new Error(`Method ${scheduleRequest.method} requires either a valid "time" or "cron" schedule`));
     }
 
-    if (method.length-1 != params.length){
-      return next(new Error('Method ' + scheduleRequest.method + 'takes ' + method.length-1 + ' params'));
+    if (method.length - 1 !== params.length) {
+      return next(new Error(`Method ${scheduleRequest.method} takes ${method.length - 1} params`));
     }
 
     // see docs for later.js if confused:
-    try{
-
-      var interval;
-      if (scheduleRequest.time){
+    try {
+      let interval;
+      if (scheduleRequest.time) {
         interval = later.parse.text(scheduleText);
       } else {
         interval = later.parse.cron(scheduleText, true);
       }
 
-      if (interval.error>-1){
+      if (interval.error > -1) {
         server.log(['hapi-method-scheduler', 'error'], { message: 'Unable to parse schedule directive', method: scheduleRequest.method });
         return next(new Error('invalid schedule'));
       }
 
       methodExecutionData.push({
-        method: method,
-        interval: interval,
-        params: params,
+        method,
+        methodName: scheduleRequest.method,
+        interval,
+        params,
         runOnStart: scheduleRequest.runOnStart
       });
-
-    } catch(exc){
+    } catch (exc) {
       server.log(['hapi-method-scheduler', 'error'], { message: 'Unable to parse schedule directive', method: scheduleRequest.method, error: exc });
       next(exc);
       return;
     }
     // check for any parse errors that didn't throw an exception:
     // push a 'done' callback to the params we pass our server methods:
-    params.push(function done(err,result){
+    params.push((err, result) => {
       if (err) {
         server.log(['hapi-method-scheduler', 'error'], { method: scheduleRequest.method, error: err });
       }
-      server.log(['hapi-method-scheduler', 'info'], { method: scheduleRequest.method, result: result });
-      onEnd(err, result);
+      server.log(['hapi-method-scheduler', 'info'], { method: scheduleRequest.method, result });
+      onEnd(err, scheduleRequest.method, result);
     });
   }
 
   // if all our methods are set up correctly then we can now put them in the queue to run:
-  server.on('start', function() {
-    _.each(methodExecutionData, function(i){
+  server.on('start', () => {
+    _.each(methodExecutionData, (i) => {
       // finally, set the methodExecutionData for our methods:
-      later.setInterval(function(){
-        onStart();
+      later.setInterval(() => {
+        onStart(i.methodName);
         i.method.apply(null, i.params);
       }, i.interval);
 
       if (i.runOnStart) {
-        onStart();
+        onStart(i.methodName);
         i.method.apply(null, i.params);
       }
     });
@@ -94,5 +97,5 @@ exports.register = function(server, options, next) {
 };
 
 exports.register.attributes = {
-    pkg: require('./package.json')
+  pkg: require('./package.json')
 };
