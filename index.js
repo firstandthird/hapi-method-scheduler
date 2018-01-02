f182a1c1f3677872e460026c85ea9d0e4cd56613
orthagonal <orthagonal1@gmail.com>
2018-01-01 23:36:43 -0600

    clean syntax, add comments

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const CronJob = require('cron').CronJob;
 const str2fn = require('str2fn');
 
-const register = async function(server, options) {
+const register = (server, options) => {
+  /* eslint-disable arrow-body-style */
   server.decorate('server', 'scheduleMethod', (cronString, methodStringOrFn, runOnInit) => {
     // this will throw an error if cronString isn't valid, which should be handled by caller:
     return new CronJob(cronString, async() => {
       try {
         // params are included in method string:
-        str2fn.execute(methodStringOrFn, server.methods);
+        await str2fn.execute(methodStringOrFn, server.methods);
       } catch (e) {
         server.log(['hapi-method-scheduler', 'error'], e);
       }
-    }, null, true, options.timezone, {}, runOnInit);
+    }, null, true, options.timezone, {}, runOnInit); // options.timezone can be left undefined
   });
+  /* eslint-enable arrow-body-style */
   options.schedule.forEach((scheduleDirective) => {
-    server.scheduleMethod(scheduleDirective.cron, scheduleDirective.method);
+    server.scheduleMethod(scheduleDirective.cron, scheduleDirective.method, scheduleDirective.runOnInit);
   });
 };
 
